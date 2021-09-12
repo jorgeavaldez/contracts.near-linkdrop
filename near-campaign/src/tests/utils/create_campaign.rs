f50d7c3fa7fdeecf978863937e474058c6fee5eb
Jorge Valdez <valdez.a.jorge@gmail.com>
2021-09-12 18:42:10 -0500

    remove unused create function; add release profile stuff

--- near-campaign/src/tests/utils/create_campaign.rs
+++ near-campaign/src/tests/utils/create_campaign.rs
@@ -3,7 +3,3 @@ use crate::*;
 pub fn create_campaign() -> Campaign {
   Campaign::new(U128::from(1_000_000_000_000_000_000_000_000), None, None, None)
 }
-
-pub fn create_campaign_with_social_network(social_network: String) -> Campaign {
-  Campaign::new(U128::from(1_000_000_000_000_000_000_000_000), None, None, Some(social_network))
-}
